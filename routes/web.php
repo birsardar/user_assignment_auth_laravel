3854ee42c4cfc317d5af0724a99d657a5c6f12ac
birsardar <100059762+birsardar@users.noreply.github.com>
2023-08-10 21:06:23 +0545

    some change

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-
+Auth::routes(); // Auth::routes(['register' => false]); // disable register
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
